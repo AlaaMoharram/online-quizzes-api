6c9d7c06a61e48424427d79d3a79d3eceb217afd
AlaaMoharram <alaa.moharram14@gmail.com>
2017-03-31 15:24:25 +0200

    allow only one correct choice per MCQ question

--- app/models/mcq_choice.rb
+++ app/models/mcq_choice.rb
@@ -4,4 +4,15 @@ class McqChoice < ApplicationRecord
 
   	# validation
   	validates_presence_of :text
+
+  	before_save :falsify_all_others
+
+  	private 
+
+  	def falsify_all_others
+		if is_correct
+			McqChoice.where.not(id: id).where(mcq_question_id: mcq_question_id).update_all(is_correct: false)
+		end
+	end
+
 end
--- spec/models/mcq_choice_spec.rb
+++ spec/models/mcq_choice_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe McqChoice, type: :model do
- # Association test
-  # ensure a choice record belongs to a single mcq_question record
-  it { should belong_to(:mcq_question) }
-  # Validation test
-  # ensure column text is present before saving
-  it { should validate_presence_of(:text) }
+	# Association test
+  	# ensure a choice record belongs to a single mcq_question record
+  	it { should belong_to(:mcq_question) }
+
+  	# Validation test
+  	# ensure column text is present before saving
+  	it { should validate_presence_of(:text) }
+
+  	# Ensure the existence of before_save callback
+  	it { should callback(:falsify_all_others).before(:save) }
+  	
 end
